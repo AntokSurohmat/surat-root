dd431aacc93a788c9718c791703685ee2ad4f1d1
Kantor PC <timohoantok@gmail.com>
2022-02-16 22:34:15 +0700

    second commit

---
+++ app/Views/admin/layouts/default.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url() ?>/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="<?= base_url() ?>/AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="<?= base_url() ?>/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>/AdminLTE/dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="<?= base_url() ?>/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="<?= base_url() ?>/AdminLTE/plugins/daterangepicker/daterangepicker.css">
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <!-- <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="<?= base_url() ?>/AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        </div> -->
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <?= $this->include('admin/layouts/navbar') ?>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <?= $this->include('admin/layouts/sidebar') ?>
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <?= $this->renderSection('content') ?>
+        </div>
+        <!-- /.content-wrapper -->
+        <footer class="main-footer">
+            <?= $this->include('admin/layouts/footer') ?>
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?= base_url() ?>/AdminLTE/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?= base_url() ?>/AdminLTE/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url() ?>/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="<?= base_url() ?>/AdminLTE/plugins/moment/moment.min.js"></script>
+    <script src="<?= base_url() ?>/AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="<?= base_url() ?>/AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+   
+    <!-- overlayScrollbars -->
+    <script src="<?= base_url() ?>/AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url() ?>/AdminLTE/dist/js/adminlte.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <!-- <script src="<?= base_url() ?>/AdminLTE/dist/js/demo.js"></script> -->
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <!-- <script src="<?= base_url() ?>/AdminLTE/dist/js/pages/dashboard.js"></script> -->
+    <!-- Costum Js -->
+    <script src="<?= base_url() ?>/custom/js/custom.js"></script>
+</body>
+
+</html>
---
+++ app/Views/admin/layouts/sidebar.php
@@ -0,0 +1,147 @@
+<!-- Brand Logo -->
+<a href="index3.html" class="brand-link">
+    <img src="<?= base_url() ?>/AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
+</a>
+
+<!-- Sidebar -->
+<div class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+    with font-awesome or any other icon font library -->
+            <li class="nav-header">NAVIGATIONS</li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item menu-open">
+                <a href="#" class="nav-link active">
+                    <i class="nav-icon fas fa-book"></i>
+                    <p>
+                        Data Master
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="./index.html" class="nav-link active">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Data Pegawai</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Pangkat & Golongan</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Jabatan</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Wilayah</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Instansi</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>SBUH</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Kode Rekening</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-envelope"></i>
+                    <p>
+                        Surat
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/layout/top-nav.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>SPT</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>SPD</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-file-pdf"></i>
+                    <p>
+                        Laporan
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/charts/chartjs.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>ChartJS</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/charts/flot.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Flot</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/charts/inline.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Inline</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/charts/uplot.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>uPlot</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                    <p>
+                        Logout
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
